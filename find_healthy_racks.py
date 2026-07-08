5f62ec09446c942e0ea1f90eb70034cf7a47c32c
Zayed Ahmed <zayed967@yahoo.com>
2026-07-08 17:05:59 -0400

    fix sql syntax

--- find_healthy_racks.py
+++ find_healthy_racks.py
@@ -1,11 +1,9 @@
 from collections.abc import Iterable
-from collections import Counter
 from datetime import datetime
 import re
 import sqlite3
 
 def find_unhealthy_racks(events: Iterable[str], threshold: int) -> list[str]:
-    hosts = {}
     conn = sqlite3.connect("racks_state.db")
     cursor = conn.cursor()
 
@@ -13,12 +11,14 @@ def find_unhealthy_racks(events: Iterable[str], threshold: int) -> list[str]:
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS hosts (
             host TEXT PRIMARY KEY,
-            timestamp REAL,
+            timestamp INTEGER,
             rack TEXT,
             stat TEXT
         )
     ''')
 
+    cursor.execute("DELETE FROM hosts")
+    conn.commit()
     pattern = re.compile(
         r"(?P<date>^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}Z)\s+"
         r"(?P<host>host-[0-9]+)\s+"
@@ -26,7 +26,9 @@ def find_unhealthy_racks(events: Iterable[str], threshold: int) -> list[str]:
         r"(?P<stat>UP|DOWN)$"
     )
 
+    counter = 0
     for event in events:
+        counter += 1
         match = pattern.fullmatch(event)
 
         date, host, rack, stat = None, None, None, None
@@ -35,38 +37,35 @@ def find_unhealthy_racks(events: Iterable[str], threshold: int) -> list[str]:
 
         try:
             date = datetime.fromisoformat(match.group('date'))
+            timestamp = int(date.timestamp())
         except ValueError:
             continue
         host = match.group('host')
         rack = match.group('rack')
         stat = match.group('stat')
         
-        cursor.execute('''
-                INSERT INTO TABLE hosts (host, timestamp, rack, stat) 
-                VALUES (?, ?, ?, ?)
-                ON CONFLICT(host) DO UPDATE SET
-                       rack = excluded.rack,
-                       stat = excluded.stat,
-                       timestamp = excluded.timestamp
-                WHERE excluded.timestamp > hosts.timestamp  
-        ''', (host, date, rack, stat))
+        cursor.execute(
+            """
+            INSERT INTO hosts (host, timestamp, rack, stat)
+            VALUES (?, ?, ?, ?)
+            ON CONFLICT(host) DO UPDATE SET
+                rack = excluded.rack,
+                stat = excluded.stat,
+                timestamp = excluded.timestamp
+            WHERE excluded.timestamp > hosts.timestamp
+            OR (
+                excluded.timestamp = hosts.timestamp
+                AND excluded.stat = 'DOWN'
+                AND hosts.stat = 'UP'
+            )
+            """,
+            (host, timestamp, rack, stat),
+        )
+        if counter >= 100_000:
+            conn.commit()
+            counter = 0
 
-        conn.commit()
-
-        # if host in hosts:
-        #     # Is this a newer event?
-        #     curr_time = hosts[host]["date"]
-        #     if date > curr_time:
-        #         hosts[host]["date"] = date
-        #         hosts[host]["stat"] = stat
-        #         hosts[host]["rack"] = rack
-        # else:
-        #     hosts[host] = { "date": date, "rack": rack, "stat": stat }
-
-    # counter = Counter()
-    # for host in hosts:
-    #     if hosts[host]["stat"] == "DOWN":
-    #         counter[hosts[host]["rack"]] += 1
+    conn.commit()
     cursor.execute('''
         SELECT rack, COUNT(*)
         FROM hosts
