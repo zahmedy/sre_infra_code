8cc7844a394ff5c69dc2f11cf38c78cba3bed6d8
Zayed Ahmed <zayed967@yahoo.com>
2026-07-08 16:06:44 -0400

    resolve RAM contrsaints with sqlite

--- find_healthy_racks.py
+++ find_healthy_racks.py
@@ -2,12 +2,22 @@ from collections.abc import Iterable
 from collections import Counter
 from datetime import datetime
 import re
+import sqlite3
 
-def find_unhealthy_racks(
-    events: Iterable[str],
-    threshold: int
-) -> list[str]:
+def find_unhealthy_racks(events: Iterable[str], threshold: int) -> list[str]:
     hosts = {}
+    conn = sqlite3.connect("racks_state.db")
+    cursor = conn.cursor()
+
+    # Create an index on host for instantaneous lookups
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS hosts (
+            host TEXT PRIMARY KEY,
+            timestamp REAL,
+            rack TEXT,
+            stat TEXT
+        )
+    ''')
 
     pattern = re.compile(
         r"(?P<date>^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}Z)\s+"
@@ -31,20 +41,42 @@ def find_unhealthy_racks(
         rack = match.group('rack')
         stat = match.group('stat')
         
-        if host in hosts:
-            # Is this a newer event?
-            curr_time = hosts[host]["date"]
-            if date > curr_time:
-                hosts[host]["date"] = date
-                hosts[host]["stat"] = stat
-                hosts[host]["rack"] = rack
+        cursor.execute('''
+                INSERT INTO TABLE hosts (host, timestamp, rack, stat) 
+                VALUES (?, ?, ?, ?)
+                ON CONFLICT(host) DO UPDATE SET
+                       rack = excluded.rack,
+                       stat = excluded.stat,
+                       timestamp = excluded.timestamp
+                WHERE excluded.timestamp > hosts.timestamp  
+        ''', (host, date, rack, stat))
 
-        else:
-            hosts[host] = { "date": date, "rack": rack, "stat": stat }
+        conn.commit()
 
-    counter = Counter()
-    for host in hosts:
-        if hosts[host]["stat"] == "DOWN":
-            counter[hosts[host]["rack"]] += 1
+        # if host in hosts:
+        #     # Is this a newer event?
+        #     curr_time = hosts[host]["date"]
+        #     if date > curr_time:
+        #         hosts[host]["date"] = date
+        #         hosts[host]["stat"] = stat
+        #         hosts[host]["rack"] = rack
+        # else:
+        #     hosts[host] = { "date": date, "rack": rack, "stat": stat }
 
-    return sorted(rack for rack, count in counter.items() if count >= threshold)
+    # counter = Counter()
+    # for host in hosts:
+    #     if hosts[host]["stat"] == "DOWN":
+    #         counter[hosts[host]["rack"]] += 1
+    cursor.execute('''
+        SELECT rack, COUNT(*)
+        FROM hosts
+        WHERE stat = 'DOWN'
+        GROUP BY rack
+        HAVING COUNT(*) >= ?
+    ''', (threshold,))
+
+    results = [row[0] for row in cursor.fetchall()]
+
+    conn.close()
+
+    return sorted(results)
