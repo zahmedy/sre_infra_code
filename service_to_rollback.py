249c5baaa2a2b5688c3c1f6e596ded743fdbd6ce
Zayed Ahmed <zayed967@yahoo.com>
2026-07-09 09:48:06 -0400

    roll back serices

---
+++ service_to_rollback.py
@@ -0,0 +1,39 @@
+from collections.abc import Iterable
+import re
+
+def services_to_rollback(
+    events: Iterable[str],
+    failure_percentage: float,
+    minimum_hosts: int,
+) -> list[str]:
+    services = {}
+
+    pattern = re.compile(r"^(service-[a-z]+)\s+(host-[0-9]+)\s+(SUCCESS|FAILURE)$")
+
+    for event in events:
+        match = pattern.fullmatch(event)
+        if not match:
+            continue
+
+        service, host, stat = match.groups()
+        services.setdefault(service, {})[host] = stat
+
+    rollback = []
+
+    for service, hosts in services.items():
+        total_hosts = len(hosts)
+
+        if total_hosts < minimum_hosts:
+            continue
+
+        failed_hosts = sum(
+            status == "FAILURE"
+            for status in hosts.values()
+        )
+
+        failure_rate = failed_hosts / total_hosts * 100
+
+        if failure_rate  >= failure_percentage:
+            rollback.append(service)
+
+    return sorted(rollback)
