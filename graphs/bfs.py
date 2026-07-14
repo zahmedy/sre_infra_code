aba1b574b4f619740026b1b3a0f89e0d97f7edd8
Zayed Ahmed <zayed967@yahoo.com>
2026-07-14 14:50:03 -0400

    bfs all

--- graphs/bfs.py
+++ graphs/bfs.py
@@ -1,6 +1,6 @@
 from collections import deque
 
-def bfs(graph: dict[str, list[int]], start: str) -> list[str]:
+def bfs(graph: dict[str, list[str]], start: str) -> list[str]:
     queue = deque([start])
     visited = {start}
     traversal = []
@@ -11,7 +11,29 @@ def bfs(graph: dict[str, list[int]], start: str) -> list[str]:
 
         for neighbor in graph[node]:
             if neighbor not in visited:
-                queue.append(node)
-                visited.add(node)
+                visited.add(neighbor)
+                queue.append(neighbor)
+
+    return traversal
+
+def bfs_all(graph: dict[str, list[str]], start: str) -> list[str]:
+    visited = set()
+    traversal = []
+
+    for start in graph:
+        if start in visited:
+            continue
+
+        queue = deque([start])
+        visited.add(start)
+
+        while queue:
+            node = queue.popleft()
+            traversal.append(node)
+
+            for neighbor in graph[node]:
+                if neighbor not in visited:
+                    queue.append(neighbor)
+                    visited.add(neighbor)
 
     return traversal
