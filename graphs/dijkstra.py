9f0d9938e91fe904ed85dcb775b8c977769b5c3d
Zayed Ahmed <zayed967@yahoo.com>
2026-07-16 10:56:47 -0400

    dijkstra

---
+++ graphs/dijkstra.py
@@ -0,0 +1,32 @@
+import heapq
+
+def shortest_latency(graph, start, target):
+    if not graph:
+        return -1
+    
+    if start == target:
+        return 0
+
+    # Start dijkstra's algorthim from start node
+    heap = [(0, start)]
+
+    # Keep track of min current distance in distance HashMap
+    distance = { start: 0 }
+
+    while heap:
+        cost, node = heapq.heappop(heap)
+
+        # If reached target it is guaranteed cost is the min latency
+        if node == target:
+            return cost
+
+        # Explore all edges from node
+        for neighbor_cost, neighbor in graph[node]:
+            new_cost = neighbor_cost + cost
+
+            # Is this route cheaper than current or skip
+            if new_cost > distance[neighbor]:
+                distance[neighbor] = new_cost
+                heapq.heappush(heap, (new_cost, neighbor))
+
+    return -1
